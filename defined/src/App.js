de2cf9a6a1ed1d6bf64147c56ec5339f6f64807b
AnhNhut2639 <zoro92211@gmail.com>
2021-12-04 15:28:31 +0700

    added feature Face Recognition by fetch image from url

--- defined/src/App.js
+++ defined/src/App.js
@@ -24,7 +24,7 @@ function App() {
   }, [file]);
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
       {image ? (
         <NewPost image={image} />
       ) : (
--- defined/src/components/NewPost.js
+++ defined/src/components/NewPost.js
@@ -6,14 +6,47 @@ function NewPost({ image }) {
   const { url, width, height } = image;
   const imgRef = useRef();
   const canvasRef = useRef();
+  // nhận dạng người trong khung hình
+
+  function LoadLabeledImages() {
+    const labels = [
+      "Black Widow",
+      "Captain America",
+      "Captain Marvel",
+      "Hawkeye",
+      "Jim Rhodes",
+      "Thor",
+      "Tony Stark",
+    ]; // giamr thoi gian phai tai lai
+    return Promise.all(
+      labels.map(async (label) => {
+        const descriptions = [];
+        for (let i = 1; i <= 2; i++) {
+          const img = await faceapi.fetchImage(
+            `https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`
+          ); // dữ liệu files ảnh
+          const detections = await faceapi
+            .detectSingleFace(img)
+            .withFaceLandmarks()
+            .withFaceDescriptor();
+          descriptions.push(detections.descriptor);
+        }
+
+        return new faceapi.LabeledFaceDescriptors(label, descriptions);
+      })
+    );
+  }
 
   const handleImage = async () => {
     // nhận dạng các khuôn mặt trên ảnh
+    const labeledFaceDescriptors = await LoadLabeledImages();
+    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
     const dections = await faceapi
       .detectAllFaces(imgRef.current, new faceapi.TinyFaceDetectorOptions())
       .withFaceLandmarks()
       .withFaceExpressions()
-      .withAgeAndGender();
+      .withAgeAndGender()
+      .withFaceDescriptors();
     // tạo lớp canvas trên ảnh
     canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(imgRef.current);
     faceapi.matchDimensions(canvasRef.current, {
@@ -26,22 +59,30 @@ function NewPost({ image }) {
       height: height,
     });
 
+    const results = resize.map((d) => faceMatcher.findBestMatch(d.descriptor)); //  nhận dạng danh tính trên ảnh
     console.log(resize);
-    // vẽ khung nhận dạng khuông mặt
-    faceapi.draw.drawDetections(canvasRef.current, resize);
-    //  nhận dạng biểu cảm trên khuôn mặt
-    faceapi.draw.drawFaceExpressions(canvasRef.current, resize);
-
-    // vẽ đồ thị trên khuôn mặt
-    // faceapi.draw.drawFaceLandmarks(canvasRef.current, resize);
-    // nhận dạng tuổi và giới tính
-    resize.forEach((detection) => {
-      const box = detection.detection.box;
+    results.forEach((result, i) => {
+      const box = resize[i].detection.box;
       const drawBox = new faceapi.draw.DrawBox(box, {
-        label: Math.round(detection.age) + ", " + detection.gender,
+        label: result.toString(),
       });
       drawBox.draw(canvasRef.current);
     });
+
+    // vẽ khung nhận dạng khuông mặt
+    // faceapi.draw.drawDetections(canvasRef.current, resize);
+    //  nhận dạng biểu cảm trên khuôn mặt
+    faceapi.draw.drawFaceExpressions(canvasRef.current, resize);
+    // vẽ đồ thị trên khuôn mặt
+    // faceapi.draw.drawFaceLandmarks(canvasRef.current, resize);
+    // nhận dạng tuổi và giới tính
+    // resize.forEach((detection) => {
+    //   const box = detection.detection.box;
+    //   const drawBox = new faceapi.draw.DrawBox(box, {
+    //     label: Math.round(detection.age) + ", " + detection.gender,
+    //   });
+    //   drawBox.draw(canvasRef.current);
+    // });
   };
 
   useEffect(() => {
@@ -52,12 +93,14 @@ function NewPost({ image }) {
         faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
         faceapi.nets.faceExpressionNet.loadFromUri("/models"),
         faceapi.nets.ageGenderNet.loadFromUri("/models"),
+        faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
       ])
         .then(handleImage)
         .catch((e) => console.log(e));
     };
     imgRef.current && loadedModels();
-  }, []);
+  });
+
   return (
     <div>
       <img
